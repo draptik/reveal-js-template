3db4209f88e6025699413dc0924eba2c5748dd72
Patrick Drechsler <patrick@pdrechsler.de>
2016-01-12 22:29:16 +0100

    install.sh added symlink example usage

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-## probably not the coolest way, but it works (http://stackoverflow.com/a/246128):
+## Current directory.
+## ...probably not the coolest way, but it works (http://stackoverflow.com/a/246128):
 currentdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ## Clone reveal.js
@@ -12,9 +13,11 @@ rm -rf reveal.js/.git
 
 cd reveal.js && npm install
 
-## Feel free to replace the copy command `cp` with symlinks..
+## Example using plain copy `cp` command:
 cp $currentdir/template/index.html $currentdir/reveal.js/index.html
 cp $currentdir/talk/talk-template.md $currentdir/reveal.js/talk-template.md
 
-
+## Example using symlinks `ln -s`:
+#ln -s $currentdir/reveal.js/index.html $currentdir/template/index.html
+#ln -s $currentdir/reveal.js/talk-template.md $currentdir/talk/talk-template.md  
 
